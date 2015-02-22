5bc8c56092104dfd154de919b7718257d358df39
n_mol <akm4@yandex.com>
2015-02-22 22:04:54 +0300

    V2

--- run_analysis.R
+++ run_analysis.R
@@ -17,32 +17,32 @@ label <- rbind(test_l, train_l)
 colnames(label) <- c("act")
 
 # Select columns with mean and standart deviation in column names
-dat_s <- dat[,grepl("-mean|-std",colnames(dat))]
+dat <- dat[,grepl("-mean|-std",colnames(dat))]
 
 # Exclud columns containing meanFreq
-dat_s1 <- dat_s[, -grep("meanFreq", colnames(dat_s))] 
+dat <- dat[, -grep("meanFreq", colnames(dat))] 
 
 # Combine subject, acivity label and set of measurements
-dat2 <- cbind(subj, label, dat_s1)
+dat <- cbind(subj, label, dat)
 
 colnames(activ) <- c("act", "activity")
 
 # Merge and clean
-m_dat <- merge(activ, dat2)
+dat <- merge(activ, dat)
 library(dplyr)
-d <- select(m_dat, -act)
+dat <- select(dat, -act)
 
 # Give column names the proper syntax
-colnames(d) <- make.names(colnames(d))
+colnames(dat) <- make.names(colnames(dat))
 
 # Make the data tidy. It is a long form of tidy data.
 library(tidyr)
-d1 <- gather(d, activity, subject, tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..)
+dat <- gather(dat, activity, subject, tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..)
 
-colnames(d1) <- c("activity", "subject", "measure", "mean")
+colnames(dat) <- c("activity", "subject", "measure", "mean")
 
-d2 <- group_by(d1, activity,subject,measure)
-tidy_dat <-summarise_each(d2, funs(mean))
+dat <- group_by(dat, activity,subject,measure)
+tidy_dat <-summarise_each(dat, funs(mean))
 
 # Process tidy data to the file "tidy_data.txt" in working directory
 write.table(tidy_dat, file = ".\\tidy_data.txt", row.name=FALSE )
